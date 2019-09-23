256a622ed2c8b2b6b9e79c10c53412df0bc7c085
firmancitranurrahman <firmancitranurrahman@gmail.com>
2019-09-23 16:50:30 +0700

    Menyelesaikan Praktikum DYNAMIC FRAGMENTS

--- app/src/main/java/polinema/ac/id/starterchapter05/MainActivity.java
+++ app/src/main/java/polinema/ac/id/starterchapter05/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import polinema.ac.id.starterchapter05.activitiies.DynamicActivity;
 import polinema.ac.id.starterchapter05.activitiies.StaticActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -22,5 +23,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void handleDynamicFragment(View view) {
+        Intent intent = new Intent(this, DynamicActivity.class);
+        startActivity(intent);
     }
 }

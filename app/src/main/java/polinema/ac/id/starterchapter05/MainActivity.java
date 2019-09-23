3708c86af18d5359614f7328bb570817142a9b01
firmancitranurrahman <firmancitranurrahman@gmail.com>
2019-09-23 15:10:32 +0700

    Menyelesaikan Praktikum EMBEDED FRAGMENTS

--- app/src/main/java/polinema/ac/id/starterchapter05/MainActivity.java
+++ app/src/main/java/polinema/ac/id/starterchapter05/MainActivity.java
@@ -2,7 +2,11 @@ package polinema.ac.id.starterchapter05;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import polinema.ac.id.starterchapter05.activitiies.StaticActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void handleStaticFragment(View view) {
+        Intent intent = new Intent(this, StaticActivity.class);
+        startActivity(intent);
+    }
+
+    public void handleDynamicFragment(View view) {
     }
 }
